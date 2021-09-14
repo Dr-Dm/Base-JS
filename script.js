88c68f0d1c43c780d69159d88046259f0a53beb6
Dr-Dm <0557586@gmail.com>
2021-09-14 08:35:05 +0300

    ДЗ 1 сделано

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// var tempC = prompt('Введите температуру в градцсах цельсия');
-// var tempF = (9 / 5) * tempC + 32;
-// alert("Темература " + tempC + " градусов по Цельсию равна " + tempF + " градусам по Фаренгейту");
+var tempC = prompt('Введите температуру в градцсах цельсия');
+var tempF = (9 / 5) * tempC + 32;
+alert("Темература " + tempC + " градусов по Цельсию равна " + tempF + " градусам по Фаренгейту");
 
 
 
-var n = 'Василий';
-var admin = n;
+var name = 'Василий';
+var admin = name;
+alert(admin);
 
-alert(n);
-var b = 1000 + "108"; 
-document.write(b);
+//1000 + "108" = 1000108
+
